824fc4af3629f84356fe18dd26b96c3cb8c47fec
devarun77 <arun.arora2504@gmail.com>
2026-05-03 16:45:12 +0530

    Required changes

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL="http://localhost:9000"
+export const BASE_URL="https://unityconnect.onrender.com"
 export const clientServer = axios.create({
     baseURL: BASE_URL,
 })
